68590412d598c39dd26211a108fc9e66e4870287
Saif eddine El hanoune <sel-hano@e1r11p3.1337.ma>
2022-11-14 23:54:42 +0100

    Components

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import Nav from './Nav'
+import Header from './Header'
+import Employees from './Employees'
+import Footer from './Footer'
+import GroupedTeamMembers from './GroupedTeamMembers'
+import NotFound from './NotFound'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DataProvider } from './context/DataContext';
+
+function App() {
+
+  return (
+    <DataProvider>
+      <Router>
+        <Nav />
+        <Header />
+			<Routes>
+			<Route path="/" element={<Employees />}>
+			</Route>
+			<Route path="/GroupedTeamMembers" element={<GroupedTeamMembers />}>
+			</Route>
+			<Route path="*" element={<NotFound />} />
+			</Routes>
+        <Footer />
+      </Router>
+    </DataProvider>
+
+  );
+}
+
+export default App;
---
+++ src/Employees.jsx
@@ -0,0 +1,24 @@
+import Teams from './Teams'
+import TeamMembers from './TeamMembers'
+
+const Employees = () => {
+
+  return (
+    <main className="container">
+		<div className="row justify-content-center">
+			<div className="col-6">
+			<Teams />
+			</div>
+		</div>
+      <hr />
+		<div className="row justify-content-center">
+			<div className="col-8">
+			<div className="card-collection">
+				<TeamMembers />
+			</div>
+			</div>
+		</div>
+    </main>
+  )
+}
+export default Employees
